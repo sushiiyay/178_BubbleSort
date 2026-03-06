18a95cc6c899c6f507681f7d453263ba1229f1ca
sushiiyay <nfa.aliya7811@gmail.com>
2026-03-06 15:39:19 +0700

    a[j + 1] = temp;

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -45,3 +45,22 @@ void display()
         cout << "Data ke - " << i << " : " << a[i] << endl;
     }
 }
+
+void BubbleSort()
+{
+    cout << "\n================" << endl;
+    cout << "Hasil Setiap Pass" << endl;
+    cout << "=================" << endl;
+    for (int pass = 1; pass < n; pass++)
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
