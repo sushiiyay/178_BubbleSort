e0750ff5224057098f19759fbda73551e938a72b
sushiiyay <nfa.aliya7811@gmail.com>
2026-03-06 15:30:03 +0700

    Menambahkan cout << endl;

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -9,6 +9,32 @@ void input()
     while (true)
     {
         cout << "Masukkan panjang array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+    else
+    {
+        cout << "Error : maksimal input array adalah 20\n";
     }
+    }
+
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++) // i++ / i = i + 1
+    {
+        cout << "Masukkan Nilai ke - " << i + 1 << " : " ;
+        cin >> a[i];
+    }
+}
+
+void display()
+{
+    cout << endl;
 
 }
