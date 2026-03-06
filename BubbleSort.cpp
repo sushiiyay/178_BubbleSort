6a0440c3c94d2fef95d2ee84116c6cc7b5430a98
sushiiyay <nfa.aliya7811@gmail.com>
2026-03-06 15:33:31 +0700

    Menambahkan cout data ke -

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -36,5 +36,12 @@ void input()
 void display()
 {
     cout << endl;
+    cout << "==========================" << endl;
+    cout << "Elemen Array telah terurut" << endl;
+    cout << "==========================" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke - " << i << " : " << a[i] << endl;
+    }
 }
